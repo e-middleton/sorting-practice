b20215af63705a47f043b29eec3318e64105cee4
Emily Middleton <emiddleton@smith.edu>
2026-03-10 12:49:54 -0400

    aesthetic cleanup, larger buttons and cleaner algo cards

--- src/components/AlgorithmListing.jsx
+++ src/components/AlgorithmListing.jsx
@@ -1,35 +1,25 @@
 import {Link} from 'react-router-dom';
-import {useState} from 'react';
 
 const AlgorithmListing = ({algorithm}) => {
-  const [showFullDescription, setShowFullDescription] = useState(false);
+
 
   let description = algorithm.description;
 
-  if (!showFullDescription) {
-    description = description.substring(0,90) + '...';
-  }
-
   return (
-    <div className='bg-white rounded-xl shadow-md relative'>
+    <Link to={`/practice-${algorithm.linkName}`} className='bg-white rounded-xl shadow-md relative hover:bg-gray-100'>
       <div className='p-4'>
         <div className='mb-6'>
           <div className='font-roboto text-sm text-gray-600 my-2'>{algorithm.complexity}</div>
           <h3 className='text-lg font-roboto font-bold'>{algorithm.name}</h3>
         </div>
 
-        <div className='mb-4 text-base font-roboto'>{ description }</div>
-        <button onClick={() => setShowFullDescription((prevState) => !prevState)} className='font-roboto text-sm text-sky-500 mb-5 hover:text-sky-700'>
-          {showFullDescription ? 'less' : 'more'}
-        </button>
+        <div className='mb-4 text-base h-20 font-roboto'>{ description }</div>
         <div className='border-b border-color-gray-600 mb-5'></div>
         <div className='flex justify-end'>
-          <Link to={`/practice-${algorithm.linkName}`} className='font-roboto bg-button hover:bg-darkButton text-white px-4 py-2 rounded-lg text-center text-sm'>
-            practice
-          </Link>
         </div>
+
       </div>
-    </div>
+    </Link>
   );
 }
 export default AlgorithmListing;
--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,7 +1,7 @@
 const Hero = ({title='Practice Sorting', subtitle='Familiarize yourself with basic sorting algorithms from Data Structures by simulating them on arrays'}) => {
   return (
     <>
-      <section className='bg-darkBlock py-20 mb-1 border-b border-darkButton'>
+      <section className='bg-darkBlock py-20 border-b border-darkButton'>
         <div className='max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 flex flex-col items-center'>
           <div className='text-center'>
             <h1 className='font-roboto text-3xl text-white font-bold md:text-5xl'> {title} </h1>
