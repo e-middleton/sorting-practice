460e5e57b871d0ebf1e70a38363cf7c3fa599a03
Emily Middleton <emiddleton@smith.edu>
2026-03-19 13:06:27 -0500

    shadow and rounded button for hover

--- src/components/AlgorithmHeading.jsx
+++ src/components/AlgorithmHeading.jsx
@@ -6,7 +6,7 @@ const AlgorithmHeading = ({ algorithmTitle }) => {
       <div className='mx-auto max-w-7xl px-2 sm:px-6 lg:px-8'>
         <div className='flex h-20 items-center justify-between'>
           <div className='flex items-center justify-center'>
-            <NavLink to='/' className='font-bold text-lightBlock text-[20px]'>Home</NavLink>
+            <NavLink to='/' className='font-bold text-lightBlock text-[20px] rounded-lg p-2 hover:shadow-lg/50'>Home</NavLink>
           </div>
           <div className="flex flex-1 justify-center">
             <h1 className='font-bold text-lightBlock text-[30px]'>{algorithmTitle}</h1>
