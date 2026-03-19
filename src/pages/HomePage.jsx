a5cce1ba39ee8588be3320149f183c73f8f9ff12
Emily Middleton <emiddleton@smith.edu>
2026-03-19 12:55:09 -0500

    fixed show all algorithms button

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,13 +1,30 @@
 import Hero from "../components/Hero";
 import AlgorithmListings from "../components/AlgorithmListings";
-import ViewAllAlgorithms from "../components/ViewAllAlgorithms";
+import Navbar from "../components/Navbar";
+import {useState} from 'react';
+
 
 const HomePage = () => {
+  let endpoint = 3;
+  const [showAll, setShowAll] = useState(false);
+
+  if (showAll) {
+    endpoint = 6;
+  }
+  
   return (
     <>
+      <Navbar/>
       <Hero />
-      <AlgorithmListings title='Available Algorithms' endpoints={[0,3]}/>
-      <ViewAllAlgorithms />   
+      <AlgorithmListings title='Available Algorithms' endpoints={[0, endpoint]}/>
+      <section className='m-auto max-w-lg my-5'>
+        <button onClick={() => setShowAll((prevState) => !prevState)} 
+        className=' mx-auto flex w-2/3 items-center justify-center block h-15 bg-black rounded-xl hover:bg-gray-700'>
+          <div className='font-roboto text-white text-bold text-base text-center'>
+            {showAll ? "Hide Algorithms" : "Show All Algorithms" }
+          </div>
+        </button>
+      </section>
     </>
   );
 }
