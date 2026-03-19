039e1186a42716552e543d84f6eea18e8b8a0af3
Emily Middleton <emiddleton@smith.edu>
2026-03-19 13:43:18 -0500

    algorithm page deletion

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,6 @@
 import {Route, createHashRouter, createRoutesFromElements, RouterProvider} from 'react-router-dom';
 import HomePage from './pages/HomePage';
 import MainLayout from './layouts/MainLayout';
-import AlgorithmsPage from './pages/AlgorithmsPage';
 import NotFoundPage from './pages/NotFoundPage';
 import SelectionSortPage from './pages/SelectionSortPage';
 import HeapSortPage from './pages/HeapSortPage';
@@ -13,7 +12,6 @@ const router = createHashRouter(
     <Route path='/' element={<MainLayout />} >
       <Route index element={<HomePage />}/>
       <Route path='/about' element={<AboutPage />}/>
-      <Route path='/algorithms' element={<AlgorithmsPage homepage='false'/>}/>
       <Route path='/practice-SelectionSort' element={<SelectionSortPage />}/>
       <Route path='/practice-HeapSort' element={<HeapSortPage />}/>
       <Route path='/practice-InsertionSort' element={<InsertionSortPage />}/>
